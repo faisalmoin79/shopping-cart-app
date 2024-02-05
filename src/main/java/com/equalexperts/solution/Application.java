031b655e96cf30ec5a56040cddf1f1509152e91b
Faisal Khan <faisal.khan@medfax.com>
2024-02-05 17:30:41 -0500

    Initial project setup with gradle and spring boot

---
+++ src/main/java/com/equalexperts/solution/Application.java
@@ -0,0 +1,15 @@
+package com.equalexperts.solution;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+
+ 
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
